57162f2139ae0e2fbea07e2764aa7fbcf1216eae
beko09 <abobakerhlal09@gmail.com>
2020-03-23 11:57:56 -0700

    edit index and add new page c-t-v and add new page who-are-we and publick-policy

--- js/main.js
+++ js/main.js
@@ -2,13 +2,11 @@ $(document).ready(function() {
   //  start open and close menu
 
   $(".open").click(function() {
-    document.getElementById("Sidenav").style.width = "350px";
-    document.getElementById("main").style.marginRight = "350px";
+    document.getElementById("Sidenav").style.width = "100%";
     document.body.style.overflowX = "hidden";
   });
   $(".closebtn").click(function() {
     document.getElementById("Sidenav").style.width = "0";
-    document.getElementById("main").style.marginRight = "0";
   });
 
   //    end menu
@@ -25,4 +23,35 @@ $(document).ready(function() {
     $("html,body").animate({ scrollTop: 0 }, 900);
   });
   //  end scroll top
+  // style header boxShadow when  the scroll == 1000
+
+  var header = $("header");
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >= 80) {
+      $(header).addClass("fixed-top");
+      $(header).css("boxShadow", "0px 0px 2.6rem #e4e3e3");
+    } else {
+      $(header).removeClass("fixed-top");
+      $(header).css("boxShadow", "none");
+    }
+  });
+  //   style header boxShadow when  the scroll == 1000
+
+  //    color active
+
+  $("header .navmenu .linknav  a").click(function() {
+    $(this).addClass("active");
+    $(this)
+      .siblings()
+      .removeClass("active");
+  });
+
+  $(" .nav-menu .link-nav  a").click(function() {
+    $(this).addClass("active");
+    $(this)
+      .siblings()
+      .removeClass("active");
+  });
+  // end color active
 });
